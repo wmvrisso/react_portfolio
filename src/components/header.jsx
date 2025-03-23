8e0612d85dc6e76e9cfc4cea761c9e97eaf56825
wmvrisso <wmvrisso@gmail.com>
2025-03-22 19:06:32 -0700

    added new images

---
+++ src/components/header.jsx
@@ -0,0 +1,20 @@
+function Header() {
+    return (
+        <header>
+        <div class="logo">Will Vrisso</div>
+        <div class="menu-toggle" id="menu-toggle">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+        </div>
+        <nav id="navbar">
+            <a href="#about">About Me</a>
+            <a href="./index.html">Portfolio</a>
+            <a href="./contact.html">Contact</a>
+            <a href="#resume">Resume</a>
+        </nav>
+        </header>
+    )
+}
+
+export default Header;
